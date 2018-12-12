78a7b3b0aeb617f6268aabd98524d2448987a17d
Dwiferdio Seagal Putra <seagalputra@gmail.com>
2018-12-12 10:03:01 +0700

    Fitting data into decision tree classifier

--- main.m
+++ main.m
@@ -5,20 +5,68 @@ close all;
 addpath('lib/mfcc/');
 addpath('lib/sap-voicebox/voicebox');
 
-% load 'data/data_pasien.mat';
 [data_audio, y] = load_data();
 %% Lakukan preprocessing
 audio_mono = {};
-for i = 1:size(data_audio,2)
+for ii = 1:size(data_audio,2)
     % konversi audio stereo ke mono
-    audio_stereo = data_audio{i};
-    audio_mono{i} = sum(audio_stereo,2) / size(audio_stereo,2);
+    audio_stereo = data_audio{ii};
+    audio_mono{ii} = sum(audio_stereo,2) / size(audio_stereo,2);
 end
 %% Ekstraksi fitur audio dengan menggunakan MFCC
-audio_mfcc = {};
+ciri = [];
 fs = 44100;
-for i = 1:size(audio_mono,2)
-    % audio_mfcc{i} = mfcc_feature(audio_mono{i}, fs, 13, 256);
-    audio_mfcc{i} = melfcc(audio_mono{i}, fs);
-    audio_mfcc{i} = reshape(audio_mfcc{i},1,[]);
+Tw = 70;           % analysis frame duration (ms)
+Ts = 10;           % analysis frame shift (ms)
+alpha = 0.97;      % preemphasis coefficient
+R = [ 0 4000 ];  % frequency range to consider
+M = 20;            % number of filterbank channels 
+C = 13;            % number of cepstral coefficients
+L = 0.6;            % cepstral sine lifter parameter
+hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
+
+disp('Loading.. Feature extraction is processing..');
+for jj = 1:size(audio_mono,2)
+    temp_audio_mfcc = mfcc(audio_mono{jj}, fs, Tw, Ts, alpha, hamming, R, M, C, L);
+    temp_audio_mfcc = temp_audio_mfcc';
+    audio_mfcc{jj} = temp_audio_mfcc;
+    fprintf('audio %s extracted..\n ',num2str(jj));
 end
+
+for kk = 1:size(audio_mfcc,2)
+    ciri_temp = [mean(audio_mfcc{kk}) std(audio_mfcc{kk}) var(audio_mfcc{kk}) skewness(audio_mfcc{kk})...
+        kurtosis(audio_mfcc{kk}) entropy(audio_mfcc{kk})];
+    ciri = [ciri; ciri_temp];
+end
+ciri = real(ciri);
+
+%% Shuffle data serta membagi data menjadi data training dan testing
+
+% Random data training dan data testing
+y = y';
+[m, n] = size(y);
+idx = randperm(m);
+y_data = y(idx,:);
+ciri_data = ciri(idx,:);
+
+% Split data training dan data testing
+X_training = ciri_data(1:100,:);
+X_testing = ciri_data(101:125,:);
+Y_training = y_data(1:100,:);
+Y_testing = y_data(101:125,:);
+
+%% Fit binary classification tree
+disp('Please wait.. Still fitting model..');
+dt = fitctree(X_training,Y_training);
+prediction = dt.predict(X_testing);
+
+%% Hitung akurasi
+for ll = 1:size(Y_testing,1)
+    if (prediction(ll) == Y_testing(ll))
+        right_label(ll) = 1;
+    else
+        right_label(ll) = 0;
+    end
+end
+akurasi = sum(right_label) / size(Y_testing,1) * 100;
+fprintf('Akurasi model : %s\n', num2str(akurasi));
--- trifbank.m
+++ trifbank.m
@@ -71,9 +71,9 @@ function [ H, f, c ] = trifbank( M, K, R, fs, h2w, w2h )
 %           [1] Huang, X., Acero, A., Hon, H., 2001. Spoken Language Processing: 
 %               A guide to theory, algorithm, and system development. 
 %               Prentice Hall, Upper Saddle River, NJ, USA (pp. 314-315).
-%
-%   Author
-%           Kamil Wojcicki, June 2011
+
+%   Author  Kamil Wojcicki, UTD, June 2011
+
 
     if( nargin~= 6 ), help trifbank; return; end; % very lite input validation
 
@@ -82,20 +82,31 @@ function [ H, f, c ] = trifbank( M, K, R, fs, h2w, w2h )
     f_high = R(2);      % upper cutoff frequency (Hz) for the filterbank 
     f_max = 0.5*fs;     % filter coefficients end at this frequency (Hz)
     f = linspace( f_min, f_max, K ); % frequency range (Hz), size 1xK
+    fw = h2w( f );
 
     % filter cutoff frequencies (Hz) for all filters, size 1x(M+2)
     c = w2h( h2w(f_low)+[0:M+1]*((h2w(f_high)-h2w(f_low))/(M+1)) );
+    cw = h2w( c );
 
-    % implements Eq. (6.140) given in [1] (for a given set of warping functions)
     H = zeros( M, K );                  % zero otherwise
     for m = 1:M 
-        k = f>=c(m)&f<=c(m+1);          % up-slope
-        H(m,k) = 2*(f(k)-c(m)) / ((c(m+2)-c(m))*(c(m+1)-c(m)));
-        k = f>=c(m+1)&f<=c(m+2);        % down-slope
-        H(m,k) = 2*(c(m+2)-f(k)) / ((c(m+2)-c(m))*(c(m+2)-c(m+1)));
-    end
 
-    % H = H./repmat(max(H,[],2),1,K);  % normalize to unit height
-    % H = H./repmat(trapz(f,H,2),1,K); % normalize to unit area (inherently done)
+        % implements Eq. (6.140) on page 314 of [1] 
+        % k = f>=c(m)&f<=c(m+1); % up-slope
+        % H(m,k) = 2*(f(k)-c(m)) / ((c(m+2)-c(m))*(c(m+1)-c(m)));
+        % k = f>=c(m+1)&f<=c(m+2); % down-slope
+        % H(m,k) = 2*(c(m+2)-f(k)) / ((c(m+2)-c(m))*(c(m+2)-c(m+1)));
 
-%%% EOF
+        % implements Eq. (6.141) on page 315 of [1]
+        k = f>=c(m)&f<=c(m+1); % up-slope
+        H(m,k) = (f(k)-c(m))/(c(m+1)-c(m));
+        k = f>=c(m+1)&f<=c(m+2); % down-slope
+        H(m,k) = (c(m+2)-f(k))/(c(m+2)-c(m+1));
+       
+   end
+
+   % H = H./repmat(max(H,[],2),1,K);  % normalize to unit height (inherently done)
+   % H = H./repmat(trapz(f,H,2),1,K); % normalize to unit area 
+
+
+% EOF
