43ffb38ef43448b5219f3cd2f765b1670563e745
seagalputra <seagalputra@gmail.com>
2019-01-18 13:15:33 +0700

    Add GUI for testing data

---
+++ gui.m
@@ -0,0 +1,175 @@
+function varargout = gui(varargin)
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 18-Jan-2019 13:01:25
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+
+% Load logo
+axes(handles.telkom_logo);
+logo = imread('logo_telkom.png');
+image(logo);
+axis off;
+axis image;
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in ekstraksi_btn.
+function ekstraksi_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to ekstraksi_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in hasil_btn.
+function hasil_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to hasil_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function hasil_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to hasil_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of hasil_txt as text
+%        str2double(get(hObject,'String')) returns contents of hasil_txt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function hasil_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to hasil_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in reset_btn.
+function reset_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to reset_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.filename_txt,'String','');
+cla(handles.file_fig);
+cla(handles.spectrogram1_fig);
+cla(handles.spectrogram2_fig);
+
+function filename_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to filename_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of filename_txt as text
+%        str2double(get(hObject,'String')) returns contents of filename_txt as a double
+
+% --- Executes during object creation, after setting all properties.
+function filename_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to filename_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in browse_btn.
+function browse_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to browse_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global data_audio;
+global filename_audio pathname_audio;
+[filename_audio, pathname_audio] = uigetfile({'*.wav'}, 'File Selector');
+handles.my_audio = strcat(pathname_audio, filename_audio);
+set(handles.filename_txt, 'string', filename_audio);
+
+data_audio = audioread(handles.my_audio);
+axes(handles.file_fig);
+plot(data_audio);
+axes(handles.spectrogram1_fig);
+spectrogram(data_audio(:,1));
+axes(handles.spectrogram2_fig);
+spectrogram(data_audio(:,2));
+
+% --- Executes on button press in play_btn.
+function play_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to play_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global data_audio;
+sound(data_audio, 44100, 16);
